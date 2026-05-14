62de7bdc28a7bd2d7303640889b68e1fbf036638
zmeireles <zmeireles@gmail.com>
2026-05-14 19:20:08 +0000

    feat: agent work on t0-1-2 (auto-committed by closer)

---
+++ packages/shared-config/prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  endOfLine: "lf",
+};
